532165e90021fc6361c89d87ddcec5fd3a103fa1
Birgitta B <bboeckel@thoughtworks.com>
2016-09-24 13:29:11 +0200

    ES6-ify

--- spec/environmentReaderSpec.js
+++ spec/environmentReaderSpec.js
@@ -1,38 +1,40 @@
 const proxyquire = require('proxyquire');
 
-describe('environmentReader', function () {
+describe('environmentReader', () => {
 
-  var environmentReader, requestMock, mockConfigData = {
+  let environmentReader;
+  let requestMock;
+  const mockConfigData = {
     testEnvironment: [
-      { id: 'backend', url: 'https://my-test-env-host/health', pattern: 'buildVersion = "([0-9a-zA-Z]{1,})"'},
+      { id: 'backend', url: 'https://my-test-env-host/health', pattern: 'buildVersion = "([0-9a-zA-Z]{1,})"' },
       { id: 'client', url: 'http://my-test-env-host/build.js', pattern: 'buildVersion = "([0-9a-zA-Z]{1,})";' }
     ]
   };
 
-  beforeEach(function() {
+  beforeEach(() => {
     requestMock = jasmine.createSpy('request');
-    var getConfigMock = jasmine.createSpy('get config');
+    const getConfigMock = jasmine.createSpy('get config');
     getConfigMock.and.returnValue(mockConfigData);
     const configMock = {
-      create: function() {
+      create: () => {
         return {
           get: getConfigMock
-        }
+        };
       }
-    }
+    };
     environmentReader = proxyquire('../server/environmentReader', {
       'request': requestMock,
       './ymlHerokuConfig': configMock
     });
   });
 
-  function mockRequestAndGetOptions(num, data) {
+  const mockRequestAndGetOptions = (num, data) => {
     const requestArgs = requestMock.calls.argsFor(num);
     requestArgs[1](undefined, {}, data);
     return requestArgs[0];
-  }
+  };
 
-  it('should send requests to the configured environmentURLs', function () {
+  it('should send requests to the configured environmentURLs', () => {
     environmentReader.checkHealthAndUpdateClients();
 
     const requestOptionsEnvironment1 = mockRequestAndGetOptions(0, 'bla buildversion = "1234" bla');
@@ -42,13 +44,13 @@ describe('environmentReader', function () {
     expect(requestOptionsEnvironment2.url).toBe('http://my-test-env-host/build.js');
   });
 
-  it('should send requests to the configured environmentURLs', function (testDone) {
+  it('should send requests to the configured environmentURLs', (testDone) => {
     const whenEnvironmentDataReceived = environmentReader.checkHealthAndUpdateClients();
 
-    const requestOptionsEnvironment1 = mockRequestAndGetOptions(0, 'bla buildVersion = "b1234" bla');
-    const requestOptionsEnvironment2 = mockRequestAndGetOptions(1, 'does not match the pattern');
+    mockRequestAndGetOptions(0, 'bla buildVersion = "b1234" bla');
+    mockRequestAndGetOptions(1, 'does not match the pattern');
 
-    whenEnvironmentDataReceived.then(function(data) {
+    whenEnvironmentDataReceived.then((data) => {
       const dataService1 = data.testEnvironment[0];
       expect(dataService1.id).toBe('backend');
       expect(dataService1.env).toBe('testEnvironment');
@@ -60,7 +62,7 @@ describe('environmentReader', function () {
       expect(dataService2.status).toBe('NOT OK');
 
       testDone();
-    }).done()
+    }).done();
   });
 
 });

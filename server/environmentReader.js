0bb859b6b6f24150af38b04e12c4584b4224769b
Birgitta B <bboeckel@thoughtworks.com>
2016-09-24 13:24:12 +0200

    ES6-ify

--- server/environmentReader.js
+++ server/environmentReader.js
@@ -1,106 +1,101 @@
 
-var http = require('http');
-var _ = require('lodash');
-var Q = require('q');
-var request = require('request');
-var configReader = require('./ymlHerokuConfig');
-var logger = require('./logger');
+const _ = require('lodash');
+const Q = require('q');
+const request = require('request');
+const configReader = require('./ymlHerokuConfig');
+const logger = require('./logger');
 
-function environmentReader() {
+const environmentReader = () => {
 
-  var envs = configReader.create('environments').get();
+  const envs = configReader.create('environments').get();
 
-  function checkUrl(checkConfig) {
-    var defer = Q.defer();
+  const checkUrl = (checkConfig) => {
+    const defer = Q.defer();
 
-    var requestOptions = {
+    const requestOptions = {
       url: checkConfig.url,
       rejectUnauthorized: false,
       timeout: 10000
     };
 
-    logger.debug("Checking", checkConfig.url);
-    request(requestOptions, function (error, response, body) {
+    logger.debug('Checking', checkConfig.url);
+    request(requestOptions, (error, response, body) => {
       if(error) {
-        defer.reject('failed to get ' + requestOptions.url, error);
+        defer.reject(`failed to get ${requestOptions.url}`, error);
       } else {
         defer.resolve(body);
       }
     });
     return defer.promise;
-  }
+  };
 
-  var parseHealthCheck = function(result, pattern) {
-    var buildVersionFromHealth = new RegExp(pattern, 'g');
-    var matchBuildVersion = buildVersionFromHealth.exec(result);
+  const parseHealthCheck = (result, pattern) => {
+    const buildVersionFromHealth = new RegExp(pattern, 'g');
+    const matchBuildVersion = buildVersionFromHealth.exec(result);
 
     if(matchBuildVersion && matchBuildVersion.length >= 2) {
       return {
         value: matchBuildVersion[1],
         status: 'OK'
       };
-    } else {
-      return {
-        status: 'NOT OK'
-      };
     }
+
+    return {
+      status: 'NOT OK'
+    };
   };
 
-  var parseResult = function(checkConfig, result) {
-    return parseHealthCheck(result, checkConfig.pattern);
-  };
+  const parseResult = (checkConfig, result) =>
+    parseHealthCheck(result, checkConfig.pattern);
 
-  var checkAllEnvironments = function() {
-    var allChecks = _.flatten(_.map(_.keys(envs), function(envKey) {
+  const checkAllEnvironments = () => {
+    const allChecks = _.flatten(_.map(_.keys(envs), (envKey) => {
 
-      var checkUrls = envs[envKey];
+      const checkUrls = envs[envKey];
 
-      return _.map(checkUrls, function(checkConfig) {
-        return checkUrl(checkConfig).then(function(result) {
-          var healthData = parseResult(checkConfig, result);
+      return _.map(checkUrls, (checkConfig) =>
+        checkUrl(checkConfig).then((result) => {
+          const healthData = parseResult(checkConfig, result);
 
           healthData.env = envKey;
           healthData.url = checkConfig.url;
           healthData.id = checkConfig.id;
           return healthData;
-        }).fail(function(message) {
+        }).fail((message) => {
           return {
             env: envKey,
             id: checkConfig.id,
             status: 'NOT OK',
             message: message
           };
-        });
-      });
-
+        })
+      );
     }));
 
     return Q.all(allChecks);
 
   };
 
-  function checkHealthAndUpdateClients() {
-    return checkAllEnvironments().then(function (environmentData) {
+  const checkHealthAndUpdateClients = () =>
+    checkAllEnvironments().then((environmentData) => {
 
-      var result = {};
-      _.each(environmentData, function(data) {
+      const result = {};
+      _.each(environmentData, (data) => {
         result[data.env] = result[data.env] || [];
         result[data.env].push(data);
       });
 
       return result;
 
-    }).fail(function(error) {
-      console.log("COULD NOT CHECK ENVIRONMENTS!", error);
-      return {error: error};
+    }).fail((error) => {
+      console.log('COULD NOT CHECK ENVIRONMENTS!', error);
+      return { error: error };
     });
-
-  }
 
   return {
     checkHealthAndUpdateClients: checkHealthAndUpdateClients
   };
 
-}
+};
 
 exports.checkHealthAndUpdateClients = environmentReader().checkHealthAndUpdateClients;

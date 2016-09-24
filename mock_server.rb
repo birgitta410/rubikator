05db8d14f842f20dc6316c9f0da2b4214e3cbd51
Birgitta B <bboeckel@thoughtworks.com>
2016-09-24 13:24:26 +0200

    Mock endpoints for environment health checks

--- mock_server.rb
+++ mock_server.rb
@@ -9,8 +9,25 @@ class SimulateElkStats < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
+class SimulateEnvironmentHealthy < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET request, response
+    response.status = 200
+    response['Content-Type'] = 'application/text'
+    response.body = 'version:1234'
+  end
+end
+
+class SimulateEnvironmentUnhealthy < WEBrick::HTTPServlet::AbstractServlet
+  def do_GET request, response
+    response.status = 200
+    response['Content-Type'] = 'application/text'
+    response.body = 'no version available'
+  end
+end
+
 server = WEBrick::HTTPServer.new :Port => 1234
 server.mount('/simulate/elk', SimulateElkStats)
+server.mount('/simulate/healthy', SimulateEnvironmentHealthy)
 
 trap('INT') {
   server.stop
